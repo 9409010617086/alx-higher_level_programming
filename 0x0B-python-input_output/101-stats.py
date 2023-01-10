970025bfcb4be9e5bd4f8d6c97bdb99f4ee47192
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2023-01-10 20:28:43 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,14 +2,12 @@
 """Reads from standard input and computes metrics
 """
 
-
 def print_stats(size, status_codes):
     """Print accumulated metrics
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
-
 
 if __name__ == "__main__":
     import sys
